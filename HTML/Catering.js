687fd02d362f6a1b7542e78fe053104492ef7904
robyr <robyradwulf@gmail.com>
2023-11-18 18:49:53 -0600

    version (1.1) fixed

--- HTML/Catering.js
+++ HTML/Catering.js
@@ -1,4 +1,4 @@
-function validateAndSubmit() {
+function validateAndSubmit(formId) {
     // Basic form validation
     var name = document.getElementById("name").value;
     var phone = document.getElementById("phone").value;
@@ -8,18 +8,15 @@ function validateAndSubmit() {
         return;
     }
 
-    // You can add more validation logic as needed
-
     // Create a FormData object to gather form data
-    const formData = new FormData(document.getElementById('cateringForm'));
+    const formData = new FormData(document.getElementById(formId));
+
+    const endpoint = 'http://localhost:3000/api/catering';
 
     // Send a POST request to the server
-    fetch('http://localhost:3000/api/catering', {
+    fetch(endpoint, {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(Object.fromEntries(formData)),
+        body: formData,
     })
         .then(response => {
             if (!response.ok) {
@@ -29,12 +26,20 @@ function validateAndSubmit() {
         })
         .then(data => {
             console.log('Success:', data);
-            // Handle success, e.g., show a success message to the user
-            alert("Form submitted successfully!");
+
+            // Check if there is a message from the server and display it
+            if (data.message) {
+                alert(data.message);
+            } else {
+                // Handle success, e.g., show a success message to the user
+                alert("Form submitted successfully!");
+            }
         })
         .catch(error => {
             console.error('Error:', error);
-            // Handle error, e.g., show an error message to the user
+
+            // Display the specific error message received from the server
+            alert("An error occurred while submitting the form. Please try again.\nError: " + error.message);
         });
 }
 
--- HTML/server.js
+++ HTML/server.js
@@ -1,13 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const multer = require('multer');
 const mysql = require('mysql');
 
 const app = express();
 const port = 3000;
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 // Create a MySQL connection
 const db = mysql.createConnection({
@@ -26,23 +24,50 @@ db.connect((err) => {
     }
 });
 
+// Multer setup
+const upload = multer();
+
+// Middlewares
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 // Endpoint to handle catering data
-app.post('/api/catering', (req, res) => {
+app.post('/api/catering', upload.none(), (req, res) => {
     const cateringData = req.body;
 
-    // Insert catering data into the MySQL database
-    const sql = 'INSERT INTO catering_requests SET ?';
+    // Extract payment data from cateringData
+    const paymentData = {
+        Card_type: cateringData.Card_type,
+        cardholder_name: cateringData.cardholder_name,
+        card_number: cateringData.card_number,
+        expiry_date: cateringData.expiry_date,
+        cvv: cateringData.cvv,
+    };
 
-    db.query(sql, cateringData, (err, result) => {
+    // Insert catering data into the MySQL database
+    const cateringSql = 'INSERT INTO catering_requests SET ?';
+    db.query(cateringSql, cateringData, (err) => {
         if (err) {
             console.error('Error saving catering data:', err);
-            res.status(500).send('Error saving catering data.');
+            res.status(500).json({ error: 'Error saving catering data.', details: err.message });
         } else {
             console.log('Catering data saved successfully.');
-            res.status(200).send('Catering data saved successfully.');
+
+            // Insert payment data into the MySQL database
+            const paymentSql = 'INSERT INTO payment_info SET ?';
+            db.query(paymentSql, paymentData, (err) => {
+                if (err) {
+                    console.error('Error saving payment data:', err);
+                    res.status(500).json({ error: 'Error saving payment data.', details: err.message });
+                } else {
+                    console.log('Payment data saved successfully.');
+                    res.status(200).json({ message: 'Catering and payment data saved successfully.' });
+                }
+            });
         }
     });
 });
+
 
 app.listen(port, () => {
     console.log(`Server listening on http://localhost:${port}`);
